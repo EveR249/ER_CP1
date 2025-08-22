76fcb89579e01a7712104064475cd32487cb8b42
EveR249 <eve.richardson@ucas-edu.net>
2025-08-22 10:40:39 -0600

    minor edit

--- projects/who_are_you.py
+++ projects/who_are_you.py
@@ -4,4 +4,4 @@ name=input("What is your name? ")
 color=input("What is your favorite color? ")
 age=input("How old are you? ")
 
-print("Hello", name, "I like", color, "too, and you must have attained so much wisdom from your", age, "years of being alive.")
+print("Hello", name, ", I like", color, "too, and you must have attained so much wisdom from your", age, "years of being alive.")
