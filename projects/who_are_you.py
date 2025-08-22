8d72b430b4610377cd7627e62d95d039c90a3a22
EveR249 <eve.richardson@ucas-edu.net>
2025-08-22 10:39:46 -0600

    Who are you

---
+++ projects/who_are_you.py
@@ -0,0 +1,7 @@
+#ER 2nd Who are you
+
+name=input("What is your name? ")
+color=input("What is your favorite color? ")
+age=input("How old are you? ")
+
+print("Hello", name, "I like", color, "too, and you must have attained so much wisdom from your", age, "years of being alive.")
