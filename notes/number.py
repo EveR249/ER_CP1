79fcca34cc5593d045568f33dbeae6121eade5f4
EveR249 <eve.richardson@ucas-edu.net>
2025-08-27 10:50:34 -0600

    notes numbers

---
+++ notes/number.py
@@ -0,0 +1,16 @@
+#ER 2nd Integers and Floats Notes
+
+#Integer
+age = 654964999999999999999999
+temp = 85
+attention = -8
+spoond = 4
+
+#Floats (floating point number)
+pi = 3.14159265358979323
+pounds = 5.6
+emotional_damage = .99
+patience_level = -.50
+
+#Math
+print(8/4+7-(8+2)%4**2)
