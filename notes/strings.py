f85ddb7e61507923a33d9c4f95387e0638610562
EveR249 <eve.richardson@ucas-edu.net>
2025-09-02 10:51:21 -0600

    notes

--- notes/strings.py
+++ notes/strings.py
@@ -1,1 +1,39 @@
 # ER 2nd Strings Notes
+
+# Examples
+
+sentence = "The quick brown fox jumps over the lazy dog"
+first_name = 'Xavier'
+month = 'September'
+food = "Chocolate"
+last_name = "LaRose"
+
+#if i need to use ' or "
+name = "La'Rose"
+book = '"The Return of The King" is the best book ever according to Ms. LaRose.'
+
+
+funny = "the dog junmped into the lake"
+
+
+# Length of strings 
+
+length = len(month)
+print("spetember has", length, "letters")
+
+
+#Escape Character
+print('\twill said \n"don\'t die"')
+
+
+#Concatenate
+full_name = first_name + " " + last_name
+
+print(full_name)
+
+# Index
+
+
+
+#Slice 
+print(last_name[2:6])
