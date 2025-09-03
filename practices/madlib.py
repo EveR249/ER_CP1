31fb60e246a22993ae07df617a5b3ada3fda9ab8
EveR249 <eve.richardson@ucas-edu.net>
2025-09-03 10:49:04 -0600

    madlibs

--- practices/madlib.py
+++ practices/madlib.py
@@ -9,13 +9,11 @@ room = input("Type a room in a house: ")
 verb_past = input("Type a past tense verb: ")
 name = input("Type the name of a family member: ")
 noun_fresh = input("Type a noun: ")
-food = input("Type a food: ")
+food = input("Type a food (plural): ")
 verb_ing = input("Type a verb ending in -ing: ")
 body = input("Type a body part (plural): ")
 plural_noun = input("Type a plural noun: ")
-verb_ing2 = verb_ing
-name2 = name
 
-madlib = "It was a "+adjective_nov+", cold November day. I woke up to the "+adjective_smell+" smell of roasting "+bird+" in the "+room+" downstairs. I "+verb_past+" down the stairs to see if I could help with the dinner. My mom told me to see if "+name+" needed a fresh "+noun_fresh+" so I carried a tray of "+food+" into the "+verb_ing+" room. When I got there, I couldn't believe my "+body+"! There were "+plural_noun+" "+verb_ing2+" on "+name+"!"
+madlib = "It was a "+adjective_nov+", cold November day. I woke up to the "+adjective_smell+" smell of roasting "+bird+" in the "+room+" downstairs. I "+verb_past+" down the stairs to see if I could help with the dinner. My mom told me to see if "+name+" needed a fresh "+noun_fresh+" so I carried a tray of "+food+" into the "+verb_ing+" room. When I got there, I couldn't believe my "+body+"! There were "+plural_noun+" "+verb_ing+" on "+name+"!"
 
 print(madlib)
