a4b225146c0de421a77531016f8b4cb8284f20b9
EveR249 <eve.richardson@ucas-edu.net>
2025-09-05 10:27:46 -0600

    idiot proof

--- practices/idiot_proof.py
+++ practices/idiot_proof.py
@@ -1,4 +1,7 @@
 #ER 2nd Idiot proofing 
 
-name = input("What is your name? ")
-phone = input("what is your phone number")
+name = input("What is your full name? ").title().strip()
+phone = input("what is your phone number? ").replace("-", " ").replace(","," ").strip()
+gpa = float(input("What is your GPA? ").strip())
+
+print(f"{name}'s phone number is {phone} and their GPA is {gpa:.1f}")
