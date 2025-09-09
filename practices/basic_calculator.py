01713bb9d5a818e0f4e4d9a45b7c7ba2f1d18259
EveR249 <eve.richardson@ucas-edu.net>
2025-09-09 10:31:37 -0600

    finished

--- practices/basic_calculator.py
+++ practices/basic_calculator.py
@@ -11,4 +11,4 @@ eq5 = num1%num2
 eq6 = num1/num2
 eq7 = num1//num2
 
-print(f"\n\nEquation 1: {num1}+{num2}={eq1:.2f}\n\nEquation 2: {num1}-{num2}={eq2:.2f}\n\nEquation 3: {num1}*{num2}={eq3:.2f}\n\nEquation 4: {num1}^{num2}={eq4:.2f}\n\nEquation 5: {num1}%{num2}={eq5:.2f}\n\nEquation 6: {num1}/{num2}={eq6:.2f}\n\nEquation 7: {num1}//{num2}={eq7:.2f}")
+print(f"\nEquation 1: {num1}+{num2}={eq1:.2f}\n\nEquation 2: {num1}-{num2}={eq2:.2f}\n\nEquation 3: {num1}*{num2}={eq3:.2f}\n\nEquation 4: {num1}^{num2}={eq4:.2f}\n\nEquation 5: {num1}%{num2}={eq5:.2f}\n\nEquation 6: {num1}/{num2}={eq6:.2f}\n\nEquation 7: {num1}//{num2}={eq7:.2f}")
