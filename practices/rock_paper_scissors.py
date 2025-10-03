d79d66c3def71b64160f60892d681e5736bd138f
EveR249 <eve.richardson@ucas-edu.net>
2025-10-03 18:46:07 +0000

    rps

--- practices/rock_paper_scissors.py
+++ practices/rock_paper_scissors.py
@@ -3,30 +3,45 @@ import random
 
 player_wins = 0
 computer_wins = 0
-while player_wins <=6 or computer_wins <=6:
+
+print("Welcome to Rock, Paper, Scissors.")
+
+while player_wins >= 0:
     comp = random.randint(1,4)
     player = int(input("Do you choose rock (1), paper (2), or scissors(3)? (Type 4 to exit)"))
     if player == 1 and comp != 2:
-        print("You Win!")
+        print("You won!")
         player_wins +=1
-        continue
-    else:
-        print("Computer Wins!")
+        print(f"You have {player_wins} wins! The computer has {computer_wins} wins!")
+    if player == 1 and comp == 2:
+        print("Computer won!")
+        computer_wins +=1
+        print(f"You have {player_wins} wins! The computer has {computer_wins} wins!")
     if player == 2 and comp != 3:
-        print("You Win!")
+        print("You won!")
         player_wins +=1
-        continue
-    else:
-        print("Computer Wins!")
+        print(f"You have {player_wins} wins! The computer has {computer_wins} wins!")
+    if player == 2 and comp == 3:
+        print("Computer won!")
         computer_wins +=1
+        print(f"You have {player_wins} wins! The computer has {computer_wins} wins!")
     if player == 3 and comp != 1:
-        print("You Win!")
+        print("You won!")
         player_wins +=1
-        continue
-    else:
-        print("Computer Wins!")
+        print(f"You have {player_wins} wins! The computer has {computer_wins} wins!")
+    if player == 3 and comp == 1:
+        print("Computer won!")
         computer_wins +=1
+        print(f"You have {player_wins} wins! The computer has {computer_wins} wins!")
     if player == 4:
+        print(f"Your total is {player_wins} wins! The computers total is {computer_wins} wins!")
         break
-else:
 
+print("Thank you for playing!")
+
+scissors = """/|  |\ 
+\|  |/
+  \/
+()  ()"""
+
+print(scissors)
