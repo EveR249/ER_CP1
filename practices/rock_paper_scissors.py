741e2290d9ec66b00e2c764e34b6b0ddf700b2a7
EveR249 <eve.richardson@ucas-edu.net>
2025-10-06 10:47:46 -0600

    actually finished

--- practices/rock_paper_scissors.py
+++ practices/rock_paper_scissors.py
@@ -22,14 +22,19 @@ computer_wins = 0
 # ()  ()"""
 
 print("Welcome to Rock, Paper, Scissors.")
-#print(f"{rock} \n {paper} \n\n {scissors}")
 
 while player_wins >= 0:
     comp = random.randint(1,4)
     player = int(input("Do you choose rock (1), paper (2), or scissors(3)? (Type 4 to exit)"))
+    if player == str or player > 4:
+        print("Invalid")
+        continue
     if player == 1 and comp != 2:
         print("You won!")
         player_wins +=1
+        print(f"You have {player_wins} wins! The computer has {computer_wins} wins!")
+    if player == 1 and comp == 1:
+        print("Tie!")
         print(f"You have {player_wins} wins! The computer has {computer_wins} wins!")
     if player == 1 and comp == 2:
         print("Computer won!")
@@ -39,6 +44,9 @@ while player_wins >= 0:
         print("You won!")
         player_wins +=1
         print(f"You have {player_wins} wins! The computer has {computer_wins} wins!")
+    if player == 2 and comp == 2:
+        print("Tie!")
+        print(f"You have {player_wins} wins! The computer has {computer_wins} wins!")
     if player == 2 and comp == 3:
         print("Computer won!")
         computer_wins +=1
@@ -46,6 +54,9 @@ while player_wins >= 0:
     if player == 3 and comp != 1:
         print("You won!")
         player_wins +=1
+        print(f"You have {player_wins} wins! The computer has {computer_wins} wins!")
+    if player == 3 and comp == 3:
+        print("Tie!")
         print(f"You have {player_wins} wins! The computer has {computer_wins} wins!")
     if player == 3 and comp == 1:
         print("Computer won!")
