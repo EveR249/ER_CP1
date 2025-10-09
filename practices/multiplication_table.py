6521071b99aac1328dc14d983e7ce756cf9039a4
EveR249 <eve.richardson@ucas-edu.net>
2025-10-09 10:49:21 -0600

    pseudocode and test

--- practices/multiplication_table.py
+++ practices/multiplication_table.py
@@ -6,3 +6,8 @@ nums = [1,2,3,4,5,6,7,8,9,10,11,12]
 for num in nums:
     print(f"{num*1} {num*2} {num*3} {num*4} {num*5} {num*6} {num*7} {num*8} {num*9} {num*10} {num*11} {num*12} ")
     time.sleep(0.5)
+
+nums = [[1,2,3],[4,5,6],[7,8,9],[10,11,12]]
+
+for list in nums:
+    print(list)
