9d60ea9a220aebdf7c720aaf74106d0487c31709
EveR249 <eve.richardson@ucas-edu.net>
2025-10-13 10:39:35 -0600

    password checker

--- projects/password_strength.py
+++ projects/password_strength.py
@@ -1,9 +1,9 @@
 #ER 2nd Password strength checker
 
-#Set strength = 5
-strength = 5
+#Set strength = 3
+strength = 4
 #set list or special characters
-special = ["!","@","#","$","%","^","&","*","(",")","_","+","-","=","[","]","{","}","|",";",":",".","<",">","?"]
+specials = ["!","@","#","$","%","^","&","*","(",")","_","+","-","=","[","]","{","}","|",";",":",".","<",">","?"]
 #Ask user for their password (input)
 password = input("Please input your password: ")
 #Find length of password using len
@@ -13,26 +13,46 @@ if length <8:
 #Strength -=1
     print("You need to make your password longer")
     strength -=1
-#If you cant find !@#$%^&*()_+-=[]{}|;:,.<>? using find
-if password is !(alphanumeric):
-#Strength -=1
-    print("You need to add special characters")
+#find an int
+number = any(char.isdigit() for char in password )
+#if no number
+if number != True:
+# take away from strength
     strength -=1
-#If you cant find an int using find
-if int not in password:
-#Strength -=1
     print("You need to have a number in your password")
-    strength -=1
-#For character in password 
-
 #Find uppercase
-
+uppercase = any(char.isupper() for char in password )
 #If no uppercase 
-
+if uppercase != True:
 #strength-=1
-
+    strength -=1
+    print("You need to have an uppercase in your password")
 #Find lowercase
-
+lowercase = any(char.islower() for char in password )
 #If no lowercase
-
+if lowercase != True:
 #Strength -=1
+    strength -=1
+    print("You need to have a lowercase in your password")
+#If you cant find !@#$%^&*()_+-=[]{}|;:,.<>? using find
+for x in specials:
+    password.find(x)
+    if x in password:
+#Strength +=1
+        strength +=1
+        break
+#else need to add
+else:
+    print("You need to add special characters")
+
+#Display password strength
+print(f"Your password strength is {strength}/5")
+#Print out strength statement for whatever value you end up with
+if strength == 5:
+    print("That is very strong!")
+elif strength == 4:
+    print("That is strong!")
+elif strength == 3:
+    print("That is moderate.")
+elif strength <= 2:
+    print("That is weak!")
