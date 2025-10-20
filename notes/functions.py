4b6c17b35682084a7a88d738c0d8843729fd501b
EveR249 <eve.richardson@ucas-edu.net>
2025-10-20 10:50:58 -0600

    notes

---
+++ notes/functions.py
@@ -0,0 +1,18 @@
+#ER 2nd functions notes
+
+def add(x,y):
+    return x+y
+
+num = 0
+while num <add(5,5):
+    print("Duck")
+    num +=1
+print("Goose")
+print(f"results are {add(6,89)}")
+total = add(6,89)
+print(add(add(3.14,.85), 10))
+add(6,89)
+add(7,5)
+add(59,7)
+add(95,5)
+add(45,8)
