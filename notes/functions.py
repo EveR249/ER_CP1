ff55171f71eb00bb77f7dee08fee2943fdfc8794
EveR249 <eve.richardson@ucas-edu.net>
2025-10-21 10:44:58 -0600

    notes

--- notes/functions.py
+++ notes/functions.py
@@ -1,9 +1,25 @@
 #ER 2nd functions notes
 
+num = 0
+player_hp = 100
+monster_hp = 100
+
 def add(x,y):
     return x+y
 
-num = 0
+def initials(name):
+    names = name.split(" ")
+    initial = ""
+    for name in names:
+        initial += name[0]
+    return initial
+
+def attack(dmg, turn):
+    if turn == "player":
+        return monster_hp - dmg, player_hp
+    else:
+        return monster_hp, player_hp-dmg, 
+
 while num <add(5,5):
     print("Duck")
     num +=1
@@ -16,3 +32,13 @@ add(7,5)
 add(59,7)
 add(95,5)
 add(45,8)
+
+print(f"Tia's initials are: {initials("Tia LaRose")}")
+print(f"Eve's initials are: {initials("Eve E Richardson")}")
+
+monster_hp, player_hp = attack(15, "monster")
+print(f"Player Health: {player_hp}")
+print(f"Monster Health: {monster_hp}")
+
+print(f"a = {ord("a")}")
+print(f"100 = {chr(100)}")
