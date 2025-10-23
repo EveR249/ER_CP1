26c102d3cbd367aefe5e99701d0d5aa9b3fb2210
EveR249 <eve.richardson@ucas-edu.net>
2025-10-23 04:54:05 +0000

    it doesn't work anymore ;-;

--- practices/caesar_cipher.py
+++ practices/caesar_cipher.py
@@ -1,11 +1,11 @@
-#ER 2nd Caesar Cypher
+#ER 2nd Caesar Cipher
 
 #create encoding function
 def encode():
     #create empty list and string
     letter_list = []
     #get the message you want to code
-    message = input("What message would you like to encode? ").upper().strip()
+    message = input("What message would you like to encode? ").strip()
     #input how far they want to shift
     shift = int(input("How far do you want to shift? "))
     #for every character in code switch to numbers
@@ -14,7 +14,9 @@ def encode():
         #add shift to the number value assigned to each letter
         new = char + shift
     #if number is greater than z restart back at a
-        if new > 90:
+        if new > 122:
+            new -=26
+        if new > 90 and new < 97:
             new -=26
         if new <= 64:
             new = char
@@ -31,7 +33,7 @@ def encode():
 def decode():
     letter_list = []
     #get the coded message
-    message = input("What message would you like to decode? ").upper()
+    message = input("What message would you like to decode? ")
     #get how far it was shifted
     shift = int(input("How far was it shifted? "))
     #for every character turn it into numbers
@@ -40,8 +42,10 @@ def decode():
     #subtract how far it shifted from all the numbers
         new = char - shift
     #make it go right from a to z
-        if new > 90:
-            new +=26
+        if new > 122:
+            new -=26
+        if new > 90 and new < 97:
+            new -=26
         if new <= 64:
             new = char
     #Turn back into letters
