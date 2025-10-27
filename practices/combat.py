e1b446b0a7cd9c7883cd4d416cf1bb043ea4a439
EveR249 <eve.richardson@ucas-edu.net>
2025-10-27 10:51:09 -0600

    combat

--- practices/combat.py
+++ practices/combat.py
@@ -7,60 +7,88 @@ monster_hp = 40
 monster_defense = 10
 run = 0
 
-def user_turn():
+def user_turn(health, monster_hp, monster_defense, run):
     print("What would you like to do?")
     if fight_class == 1:
-        move = int(input("1. Normal Attack \n 2. Drink healing potion (regain 9 health) \n 3. Flee (may or may not get away) \n"))
+        move = int(input("1. Normal Attack \n 2. Drink healing potion (regain 9 health) \n 3. Flee (may or may not get away) \n 4. Wild attack (More powerful, but damages you) \n"))
         if move == 1:
-            attack = random.randint(1,21) + 3
-            dmg = random.randint(1,9) + 4
+            attack = random.randint(1,20) + 3
+            dmg = random.randint(1,8) + 4
             if attack > monster_defense:
-                goblin_hp -= dmg
-                print(f"You hit! The Goblin now has {goblin_hp} health left!")
+                monster_hp = monster_hp - dmg
+                return print(f"You hit! The Goblin now has {monster_hp} health left!")
             else:
-                print("You missed!")
+                return print("You missed!")
         elif move == 2:
-            health  = health +9
+            health +=9
+            return print(f"Your health is now {health}")
         elif move == 3:
-            run = random.randint(1,21)
-            if run >12:
-                print("You escaped!")
+            run = random.randint(1,20)
+            if run >16:
+                return print("You escaped!")
+            return run
+        elif move == 4:
+            attack = random.randint(1,20) + 3
+            dmg = random.randint(1,8) + 8
+            if attack > monster_defense:
+                monster_hp = monster_hp - dmg
+                health = health - 4
+                return print(f"You hit! The Goblin now has {monster_hp} health left! \n You have {health} health left!")
+                
+            else:
+                return print("You missed!")
     elif fight_class == 2:
-        move = int(input("1. Normal Attack (must skip a turn to charge) \n 2. Drink healing potion (regain 9 health) \n 3. Flee (may or may not get away)"))
+        move = int(input("1. Normal Attack (must skip a turn to charge) \n 2. Drink healing potion (regain 9 health) \n 3. Flee (may or may not get away) \n 4. Wild attack (More powerful, but damages you \n"))
         if move == 1:
             monster_turn()
-            attack = random.randint(1,21) + 4
-            dmg = random.randint(1,9) + 5
+            attack = random.randint(1,20) + 4
+            dmg = random.randint(1,8) + 5
             if attack > monster_defense:
-                goblin_hp -= dmg
-                print(f"You hit! The Goblin now has {goblin_hp} health left!")
+                monster_hp = monster_hp - dmg
+                print(f"You hit! The Goblin now has {monster_hp} health left!")
             else:
                 print("You missed!")
         elif move == 2:
             health +=9
+            return print(f"Your health is now {health}")
         elif move == 3:
-            run = random.randint(1,21)
-            if run >12:
+            run = random.randint(1,20)
+            if run >16:
                 print("You escaped!")
+        elif move == 4:
+            attack = random.randint(1,20) + 5
+            dmg = random.randint(1,8) + 9
+            if attack > monster_defense:
+                monster_hp = monster_hp - dmg
+                health = health - 5
+                return print(f"You hit! The Goblin now has {monster_hp} health left! \n You have {health} health left!")
     else:
-        move = int(input("1. Normal Attack \n 2. Drink healing potion (regain 9 health) \n 3. Flee (may or may not get away)"))
+        move = int(input("1. Normal Attack \n 2. Drink healing potion (regain 9 health) \n 3. Flee (may or may not get away) \n 4. Wild attack (More powerful, but damages you \n"))
         if move == 1:
-            attack = random.randint(1,21) + 5
-            dmg = random.randint(1,9) + 3
+            attack = random.randint(1,20) + 5
+            dmg = random.randint(1,8) + 3
             if attack > monster_defense:
-                goblin_hp -= dmg
-                print(f"You hit! The Goblin now has {goblin_hp} health left!")
+                monster_hp = monster_hp - dmg
+                print(f"You hit! The Goblin now has {monster_hp} health left!")
             else:
                 print("You missed!")
         elif move == 2:
             health +=9
+            return print(f"Your health is now {health}")
         elif move == 3:
-            run = random.randint(1,21)
-            if run >12:
+            run = random.randint(1,20)
+            if run >16:
                 print("You escaped!")
+        elif move == 4:
+            attack = random.randint(1,20) + 5
+            dmg = random.randint(1,8) + 7
+            if attack > monster_defense:
+                monster_hp = monster_hp - dmg
+                health = health - 3
+                return print(f"You hit! The Goblin now has {monster_hp} health left! \n You have {health} health left!")
 
-def monster_turn():
-    monster_attack = random.randint(1,21) +2
+def monster_turn(health, defense):
+    monster_attack = random.randint(1,20) +2
     if monster_attack > defense:
         health = health - monster_attack
         print(f"The Goblin clobbered you! Your health is now {health}")
@@ -71,7 +99,7 @@ def monster_turn():
         
 
 print("Welcome to training! First I need to know some things about you!")
-name = input("What is your name? ")
+name = input("What is your name? ").strip().capitalize()
 
 while True:
     fight_class = int(input("What class of fighter are you? \n 1. Fighter \n 2. Mage \n 3. Rogue \n"))
@@ -108,18 +136,18 @@ while True:
 
 print("You are being attacked by Goblin!")
 
-turn = random.randint(1,3)
+turn = random.randint(1,2)
 
 while health >0 and monster_hp >0:
     if run > 16:
         break
     elif turn == 1:  
         print("Your turn!")
-        user_turn()
+        user_turn(health, monster_hp, monster_defense, run)
         turn +=1
     else:
         print("The Goblin's turn!")
-        monster_turn()
+        monster_turn(health, defense)
         turn -=1
 else:
     if monster_hp == health:
