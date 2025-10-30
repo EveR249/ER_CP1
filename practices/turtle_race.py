d1aa6cf2b49ff1e6a561a8e0ba70b3ced76a263e
EveR249 <eve.richardson@ucas-edu.net>
2025-10-30 10:51:29 -0600

    turtle

--- practices/turtle_race.py
+++ practices/turtle_race.py
@@ -1,8 +1,8 @@
 #ER 2nd Turtle race
 import turtle
 import random
+
 def race():
-    for x in range (50):
         pink.forward(random.randint(10,40))
         purple.forward(random.randint(10,40))
         yellow.forward(random.randint(10,40))
@@ -10,90 +10,81 @@ def race():
         blue.forward(random.randint(10,40))
 
 #create finish line
-turtle.hideturtle()
-turtle.pensize(20)
-turtle.penup()
-turtle.setx(400)
-turtle.pendown()
-turtle.sety(500)
-turtle.sety(-500)
+def finish():
+    turtle.hideturtle()
+    turtle.pensize(20)
+    turtle.penup()
+    turtle.setx(400)
+    turtle.pendown()
+    turtle.sety(500)
+    turtle.sety(-500)
+
+#set turtles
+def create(turt_name):
+     turt_name.shape("turtle")
+     turt_name.shapesize(3)
+     turt_name.pensize(5)
+     turt_name.penup()
+
 
 #Set pink turtle
 pink = turtle.Turtle()
-pink.fillcolor("pink")
-#set shape to turtle
-pink.shape("turtle")
-#set size to turtle
-pink.shapesize(3)
-#pen up
-pink.penup()
+create(pink)
+pink.color("pink")
 #Go to start
 pink.goto(-700,400)
 pink.pendown()
-pink.color("pink")
-pink.pensize(5)
 
-#set purple turtle
+#Set purple turtle
 purple = turtle.Turtle()
-purple.fillcolor ("purple")
-#set shape to turtle
-purple.shape("turtle")
-#set size to turtle
-purple.shapesize(3)
-#pen up
-purple.penup()
+create(purple)
+purple.color("purple")
 #Go to start
 purple.goto(-700,200)
 purple.pendown()
-purple.color("purple")
-purple.pensize(5)
 
-#set yellow turtle
+#Set yellow turtle
 yellow = turtle.Turtle()
-yellow.fillcolor ("yellow")
-#set shape to turtle
-yellow.shape("turtle")
-#set size to turtle
-yellow.shapesize(3)
-#pen up
-yellow.penup()
+create(yellow)
+yellow.color("yellow")
 #Go to start
 yellow.goto(-700,0)
 yellow.pendown()
-yellow.color("yellow")
-yellow.pensize(5)
 
-#set green turtle
+#Set green turtle
 green = turtle.Turtle()
-green.fillcolor ("green")
-#set shape to turtle
-green.shape("turtle")
-#set size to turtle
-green.shapesize(3)
-#pen up
-green.penup()
+create(green)
+green.color("green")
 #Go to start
 green.goto(-700,-200)
 green.pendown()
-green.color("green")
-green.pensize(5)
 
-#set blue turtle
+#Set blue turtle
 blue = turtle.Turtle()
-blue.fillcolor ("blue")
-#set shape to turtle
-blue.shape("turtle")
-#set size to turtle
-blue.shapesize(3)
-#pen up
-blue.penup()
+create(blue)
+blue.color("blue")
 #Go to start
 blue.goto(-700,-400)
 blue.pendown()
-blue.color("blue")
-blue.pensize(5)
 
-
+#put in loop and check win
 while True:
+    turtle.onclick(finish())
     turtle.onclick(race())
-    turtle.done()
+    if pink.xcor() >= 400:
+        print("Pink Won!")
+        break
+    if purple.xcor() >= 400:
+        print("Purple Won!")
+        break
+    if yellow.xcor() >= 400:
+        print("Yellow Won!")
+        break
+    if green.xcor() >= 400:
+        print("Green Won!")
+        break
+    if blue.xcor() >= 400:
+        print("Blue Won!")
+        break
+
+turtle.done()
