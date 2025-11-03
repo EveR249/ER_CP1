27c0e6f9f027fcb668663e9c114a99e671e668da
EveR249 <eve.richardson@ucas-edu.net>
2025-11-03 10:50:55 -0700

    maze

--- practices/maze_generator.py
+++ practices/maze_generator.py
@@ -2,7 +2,18 @@
 import turtle
 import random
 
-#create empty maze
-maze = [[],[],[],[],[],[]]
+#create respective wall lists
+row_wall = ["|"," "]
+col_wall = ["_"," "]
 
-#
+#create empty maze
+row_grid = [[random.choice(row_wall),random.choice(row_wall),random.choice(row_wall),random.choice(row_wall),random.choice(row_wall),random.choice(row_wall)],
+            [random.choice(row_wall),random.choice(row_wall),random.choice(row_wall),random.choice(row_wall),random.choice(row_wall),random.choice(row_wall)],
+            [random.choice(row_wall),random.choice(row_wall),random.choice(row_wall),random.choice(row_wall),random.choice(row_wall),random.choice(row_wall)],
+            [random.choice(row_wall),random.choice(row_wall),random.choice(row_wall),random.choice(row_wall),random.choice(row_wall),random.choice(row_wall)],
+            [random.choice(row_wall),random.choice(row_wall),random.choice(row_wall),random.choice(row_wall),random.choice(row_wall),random.choice(row_wall)],
+            [random.choice(row_wall),random.choice(row_wall),random.choice(row_wall),random.choice(row_wall),random.choice(row_wall),random.choice(row_wall)]]
+
+col_grid = []
+for row in row_grid:
+    print(row)
