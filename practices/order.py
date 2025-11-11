f0c14500b6965d90749f2c37be79b3be648bc20b
EveR249 <eve.richardson@ucas-edu.net>
2025-11-11 10:51:27 -0700

    menu

--- practices/order.py
+++ practices/order.py
@@ -1,4 +1,62 @@
 #ER 2nd Order Up
 
 #Make different dictionaries for each part of the meal (entree, drink, appetizer, side, dessert)
-#inside each list is 5 options and prices associated with them
+#inside each dictionary is 5 options and prices associated with them
+
+drink = {
+    "Water" : 1.25,
+    "Sprite" : 1.75,
+    "7-UP" : 1.65,
+    "Lemon-Lime Shasta" : 1.50,
+    "Sprint (Carbonated Lemon Water)" : 2.00
+}
+
+appetizer = {
+    "Pretzel Bites" : 5.25,
+    "Cheesecake Factory Brown Bread" : 7.00,
+    "Chips and Guacamole" : 4.75,
+    "Caprese Skewers" : 5.00,
+    "Deviled Eggs" : 4.50
+}
+
+entree = {
+    "Grilled Cheese and Tomato Soup" : 9.25,
+    "Margherita Pizza" : 10.75,
+    "Vegetable Masala with Rice" : 10.65,
+    "Mashed Potatoes and Gravy" : 9.50,
+    "Sweet Waffle with Berries" : 10.00
+}
+
+side = {
+    "French Fries" : 1.25,
+    "Waffle Fries" : 1.75,
+    "Curly Fries" : 1.65,
+    "British Chips" : 1.50,
+    "Smashed Potatoes" : 2.00
+}
+
+dessert = {
+    "Raspberry Cronut" : 5.25,
+    "Chocolate Cake" : 6.75,
+    "Pudding Pie" : 7.65,
+    "Donut Tower" : 14.50,
+    "Oreos" : 6.00
+}
+
+#print selection for main
+#user input for main, print out main, stupid proof, add price of selelction to total. 
+#check if they even said something on the menu if they didnt, make them
+
+#print selection for appetizer
+#user input for appetizer, print out appetizer, stupid proof, add price of selelction to total. 
+#check if they even said something on the menu if they didnt, make them
+
+#print out drink options
+#user input for drink, print out print, stupid proof, add price of selelction to total. 
+#check if they even said something on the menu if they didnt, make them
+
+#print out dessert options
+#user input for dessert, print out dessert, stupid proof, add price of selelction to total. 
+#check if they even said something on the menu if they didnt, make them
+
+#print out their receipt and total
