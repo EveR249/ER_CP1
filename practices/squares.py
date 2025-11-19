36ce07ee4e5419b0e73dd41197106e9f7effb86e
EveR249 <eve.richardson@ucas-edu.net>
2025-11-19 10:39:06 -0700

    squares

--- practices/squares.py
+++ practices/squares.py
@@ -1,7 +1,9 @@
 #ER 2nd Squared numbers
 numbers = [3, 7, 12, 25, 30, 45, 50, 65, 70, 85, 90, 105, 110, 125, 130, 145, 150, 165, 170, 185, 190, 205, 210, 225, 230]
 
+#squaring the numbers in a lambda and calling it in a map
 squared = list(map(lambda num: num*num, numbers))
 
+#print result
 for i, num in enumerate(numbers):
     print(f"Original:{num} Squared: {squared[i]}")
